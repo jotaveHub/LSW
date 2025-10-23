2f3f02a8a78d677534c45b940d36d23093a13add
jotaveHub <accurategx@gmail.com>
2025-10-23 14:40:25 -0300

    Renomeando nome das Pastas

---
+++ Aula00/script.js
@@ -0,0 +1,2 @@
+let nome = "fabio";
+console.log(nome.toUpperCase());
--- Aula04/script.js
+++ Aula04/script.js
@@ -1,88 +1,18 @@
-/*function soma(a,b) {
-    let resultado = a + b;
-    return resultado;
-}
-console.log(soma(4, 4));*/
+//let x = new Array();
 
-//função expressa
-/*const somar = function(a,b) {
-    return a+b;
-}*/
+/*const frutas = ["maça","laranja","banana"];
+const primeiro = frutas[0];
 
-// Funções Expressal de alta ordem (callback)
+console.log(primeiro);*/
 
-/* function greeting(name) {
-    alert('Hello' + name);
-}
+//let matriz = [[1,2,3],[1,2,3],[1,2,3]];
 
-function processUperInput(callback) {
-    var name = prompt('Please enter your name');
-    callback(name);
-}
-processUperInput(greeting);
-*/
+let array = ["joao","maria","jose","rosa","tico"];
+array.push("ana");
+array.unshift("carlos");
 
-/*
-function ExeCallback(callback) {
-    let resultado = callback(2,3)
-    console.log(resultado)
-}
-ExeCallback(soma);*/
+array.pop();
+array.forEach(nome => console.log(nome));
 
-
-// arrow function
-//const soma = (a,b) => a + b;
-
-/*function imprimirUsuario(nome) {
-    let resultado = `olá ${nome}`
-    console.log(resultado)
-}
-function ExibirUsuario(dados,callback) {
-    callback(dados.nome)
-}
-
-const pessoaDados = {
-    nome: "Fabio",
-    idade: 40
-}
-ExibirUsuario(pessoaDados,imprimirUsuario) */
-
-/*
-let nomeX = "Fabio"
-function ExibirNome() {
-    let nomeX = "alice";
-    console.log(nomeX);
-}
-
-ExibirNome();
-console.log(nomeX)
-*/
-
-// Funções Aninhadas 
-/*
-function saudacao(nome) {
-    function mensagem() {
-        return `Olá, ${nome}! Seja bem-vindo.`;
-    }
-    return mensagem();
-}
-console.log(saudacao("Maria"));
-*/
-
-function potenciacao(a,b=2) {
-    let resultado = 1;
-    if(b >= 3) {
-        for(let i =0; i <b; i++) {
-            resultado = resultado * a;
-        }
-    } else {
-        resultado = a * a;
-    }
-    return resultado;
-}
-
-console.log(potenciacao(2));
-console.log(potenciacao(2,4));
-
-
-
+const nomesMaiusculos = array.map(nome => nome.toUpperCase());
+console.log(nomesMaiusculos);
